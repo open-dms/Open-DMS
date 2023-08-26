a182e3de336b55f363ed6afb229c84a420920acf
herrhelms <helms@straw.io>
2023-08-27 01:18:56 +0200

    feat(data): extend paginatedFetch helper

--- apps/odms-app/src/lib/server/fetch.ts
+++ apps/odms-app/src/lib/server/fetch.ts
@@ -1,22 +1,26 @@
 "server only";
 
-/** @note default timeout to enforce revalidation after */
-const paginatedFetchRevalidationTimeout = 604800;
+/** @note timeout to enforce revalidation after (set to 1 day) */
+const paginatedFetchRevalidationTimeout = 86400;
 
 export interface PaginationArgs {
-  limit: number;
-  offset: number;
-  page: number | 0;
-  sort: Array<string>;
+  limit?: number | undefined;
+  offset?: number | undefined;
+  page?: number | 0;
+  sort?: Array<string> | ["dateCreated", "ASC"];
 }
 
-/** @note API fetch function */
-/** with configurable cache expiry and ability to force revalidate */
+/** @note app wide API fetch function */
+/** with configurable cache expiry & ability to force-revalidate */
 /** see: https://nextjs.org/docs/app/api-reference/functions/revalidateTag) */
 
 export const paginatedFetch = async (path: string, entity: string, params: PaginationArgs) => {
   /** @todo add better validation for passed props */
-  const apiRequestURI = process.env.NEXTAUTH_URL + path;
+
+  const searchParams = new URLSearchParams(params as Record<string, string>);
+  const searchString = searchParams.toString();
+
+  const apiRequestURI = process.env.NEXTAUTH_URL + path + searchString;
   return await fetch(apiRequestURI, {
     next: { tags: [entity], revalidate: paginatedFetchRevalidationTimeout },
   });
