d9349dba9ecfe6b9c70ea3f62973934166fce926
herrhelms <helms@straw.io>
2023-08-26 17:55:59 +0200

    feat(setup): ensure tailwind works as expected

--- apps/odms-app/app/layout.tsx
+++ apps/odms-app/app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+/** @note we're importing a shared globals.css from the dedicated `odms-ui` package (includes @taiwind imports)  */
+
+import "odms-ui/app/globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="w-screen h-screen flex items-center justify-center">{children}</body>
     </html>
   );
 }
