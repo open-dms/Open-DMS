3312d36aa3ab88702070cb78d03da131cf44a0e9
herrhelms <helms@straw.io>
2023-08-27 13:25:28 +0200

    feat(data): unified JSONResponse helper for API route handler

--- apps/odms-app/src/lib/server/response.ts
+++ apps/odms-app/src/lib/server/response.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 
-export const JSONResponse = (data: Array<any> | Record<string, any>) => {
+export const JSONResponse = (data: Array<any> | Record<string, any>, options: Record<string, any>) => {
   if (Array.isArray(data)) {
-    return NextResponse.json({ res: data, _meta: { total: data.length } });
+    return NextResponse.json({ res: data, _meta: { total: data.length, ...options } });
   }
-  return NextResponse.json({ res: [data], _meta: { total: 1 } });
+  return NextResponse.json({ res: [data], _meta: { total: 1, ...options } });
 };
