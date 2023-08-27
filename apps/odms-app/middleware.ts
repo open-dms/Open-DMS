929d7237701e3410d76cdffc92889317d85cb641
herrhelms <helms@straw.io>
2023-08-27 19:45:26 +0200

    feat(data): added a 'middleware.ts' + import path fix

---
+++ apps/odms-app/middleware.ts
@@ -0,0 +1,38 @@
+import { fallbackLng, languages } from "./src/lib/i18n/i18n.config";
+
+import { NextResponse } from "next/server";
+import acceptLanguage from "accept-language";
+
+acceptLanguage.languages(languages);
+
+export const config = {
+  matcher: "/:lng*",
+  // matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)"],
+};
+
+const cookieName = "i18next";
+
+export function middleware(req) {
+  let lng: any;
+  if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName).value);
+  if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"));
+  if (!lng) lng = fallbackLng;
+
+  // Redirect if lng in path is not supported
+  if (
+    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
+    !req.nextUrl.pathname.startsWith("/_next")
+  ) {
+    return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url));
+  }
+
+  if (req.headers.has("referer")) {
+    const refererUrl = new URL(req.headers.get("referer"));
+    const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`));
+    const response = NextResponse.next();
+    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+    return response;
+  }
+
+  return NextResponse.next();
+}
--- apps/odms-app/src/lib/i18n/i18n.init.ts
+++ apps/odms-app/src/lib/i18n/i18n.init.ts
@@ -7,11 +7,7 @@ export const initI18next = async (language: string, ns: string) => {
   const i18nInstance = createInstance();
   await i18nInstance
     .use(initReactI18next)
-    .use(
-      resourcesToBackend(
-        (language: string, namespace: string) => import(`../lib/i18n/locales/${language}/${namespace}.json`)
-      )
-    )
+    .use(resourcesToBackend((language: string, namespace: string) => import(`./locales/${language}/${namespace}.json`)))
     .init(i18nConfig(language, ns));
   return i18nInstance;
 };
