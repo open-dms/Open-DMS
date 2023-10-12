62d57a02231659cf8eb82c8af9248110b8c78367
herrhelms <helms@straw.io>
2023-10-12 15:03:54 +0200

    chore(setup): update monorepo settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  parser: '@typescript-eslint/parser', 
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "custom"
+  ],
+  plugins: ["@typescript-eslint"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
