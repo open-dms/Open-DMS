0ea5365484bc6de1da8004b9e8c6ffc29cbc9f95
herrhelms <helms@straw.io>
2023-10-12 15:04:54 +0200

    chore(setup): update nextjs config & dev settings

--- apps/odms-app/.eslintrc.js
+++ apps/odms-app/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser', 
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended", "custom"],
+  plugins: ["@typescript-eslint"],
   extends: ["custom"],
 };
+
+
--- apps/odms-app/next.config.js
+++ apps/odms-app/next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["odms-ui"],
+  experimental: {
+    serverActions: true,
+  },
 };
