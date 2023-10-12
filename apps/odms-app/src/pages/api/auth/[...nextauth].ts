263a9ce16ed34c49c111c69e91eb0210883a921d
herrhelms <helms@straw.io>
2023-10-12 15:08:02 +0200

    feat(auth): next-auth & prisma schema

--- apps/odms-app/src/pages/api/auth/[...nextauth].ts
+++ apps/odms-app/src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import NextAuth, { type AuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../lib/server/db";
+import { User } from "@prisma/client";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -16,8 +17,8 @@ export const authOptions: AuthOptions = {
       },
       async sendVerificationRequest({ identifier: email, url }) {
         /** @todo run check against db that email is allowed to onboard (e.g. invites) */
-
         /** @note We're not sending any emails anywhere for now */
+
         /** The magic-link to sign-in users will be output to console only. * /
         /** Manually click the link from console and the user sign-in and session creation will succeed! */
 
@@ -30,6 +31,34 @@ export const authOptions: AuthOptions = {
     strategy: "database",
     maxAge: 30 * 24 * 60 * 60, // 30 days
     updateAge: 12 * 60 * 60, // 12 hours
+  },
+  callbacks: {
+    // async signIn({ user, account, profile, email, credentials }) { return true },
+    // async redirect({ url, baseUrl }) { return baseUrl },
+    async session({ session, token, user }) {
+      if (!session.user.emailAdded) {
+        session.user.emailAdded =
+          (user as User).emailAdded !== null ? true : false;
+      }
+
+      if (!session.user.emailVerified) {
+        session.user.emailVerified =
+          (user as User).emailVerified !== null ? true : false;
+      }
+      if (!session?.user?.uuid) {
+        session.user.uuid = (user as User).uuid;
+      }
+      return session;
+    },
+    // async jwt({ token, user, account, profile, isNewUser }) { return token }
+  },
+
+  pages: {
+    signIn: "/de/join",
+    signOut: "/de",
+    error: "/de/join",
+    verifyRequest: "/de/join/get-verified",
+    newUser: "/de/join/get-started",
   },
 };
 
--- apps/odms-app/src/types/next-auth.d.ts
+++ apps/odms-app/src/types/next-auth.d.ts
@@ -1,9 +1,11 @@
-import { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       uuid: string;
+      emailAdded: boolean | null;
+      emailVerified: boolean | null;
     } & DefaultSession["user"];
   }
 }
