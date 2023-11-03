7b4defb4c20224b2cda24f5f757213e632b8635c
herrhelms <helms@straw.io>
2023-11-03 16:39:31 +0100

    fix(app): page flickering during page loading when signed out

--- apps/odms-app/app/[lng]/loading.tsx
+++ apps/odms-app/app/[lng]/loading.tsx
@@ -1,9 +1,19 @@
 import Loading from "@/src/components/layout/Loading";
+import Sidebar from "@/src/components/AppSide";
 
-export default function DefaultLoading() {
+export default function DefaultLoading({ lng }: { lng: string | "de" }) {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <Loading message="..." />
+    <div id="page-wrapper" className="flex w-full h-full flex-col md:flex-row">
+      {/* @ts-ignore Server Component */}
+      <Sidebar language={lng} session={undefined} />
+      <div
+        id="page-content"
+        className="flex w-full md:w-1/2 h-full  my-auto flex-col justify-center items-center"
+      >
+        <div className="w-full h-full flex items-center justify-center">
+          <Loading message="..." />
+        </div>
+      </div>
     </div>
   );
 }
